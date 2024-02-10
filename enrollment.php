0390fe958b256ac996ff2354e2b01dbf2c0aa69d
John Paul Santos <johnpaulk24@gmail.com>
2024-02-10 23:38:57 +0800

    added bootstrap 5

--- enrollment.php
+++ enrollment.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/js/bootstrap.bundle.js"></script>
     <style>
         .enrollment-container {
             max-width: 5400px;
